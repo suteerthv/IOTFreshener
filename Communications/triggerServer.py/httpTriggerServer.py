88ea028ac7d3988f31a5c70adf2df401fe308007
suteerth <svishnu@umd.edu>
2016-02-25 21:40:35 -0500

    changes the API call procedure

--- Communications/triggerServer.py/httpTriggerServer.py
+++ Communications/triggerServer.py/httpTriggerServer.py
@@ -6,12 +6,25 @@ class MyHandler(BaseHTTPRequestHandler):
 		qs={}
 		path = self.path
 		if '?' in path:
-			path, tmp = path.split('?',1)
-			qs = urlparse.parse_qs(tmp)
+			#path, tmp =
+			print path.split('?')
+	#		print tmp
+#			qs = urlparse.parse_qs(tmp)
 		print path,qs
+#		time = (qs['ntimes'][0:0])
+#		device = (qs['device'][0:0])
+	#	processGet(time,device)		
+		print qs
+	
 		print "End of parsing get"
+	def processGet(t,d):
+		print "time:"%t
+		print "device"%d
+				
+		
+	
 	def log_request(self, code=None, size=None):
-		print('Request')
+		print('Req')
 if __name__ == "__main__":
 	try:
 		server = HTTPServer(('localhost', 8080), MyHandler)
@@ -20,3 +33,4 @@ if __name__ == "__main__":
 	except KeyboardInterrupt:
 		print('^C received, shutting down server')
 		server.socket.close()
+
