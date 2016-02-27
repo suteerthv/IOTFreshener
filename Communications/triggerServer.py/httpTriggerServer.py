3f1efe8d40ca1482f1c09c16260a48ac919eb906
suteerth <svishnu@umd.edu>
2016-02-27 03:58:24 -0500

    triggerV1.0

--- Communications/triggerServer.py/httpTriggerServer.py
+++ Communications/triggerServer.py/httpTriggerServer.py
@@ -8,11 +8,12 @@ import json
 
 HOST='0.0.0.0'
 PORT=8080
-
+#(deviceID,)
 URL="https://api.particle.io/v1/devices/{!s}/led/"
 DEVICES = ['51ff6f065082554910260887']
 ACCESS_TOKEN="2228662ce1e5a04e7fb21f9b81aa3bc390b72506"
-COMMAND = "command=on"
+
+
 class MyHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		print("Just received a GET request")
@@ -34,8 +35,8 @@ class MyHandler(BaseHTTPRequestHandler):
 		self.wfile.write("<body><p>%s</p>"%con)
 		self.wfile.write("</body></html>")
 		print qs
-	
 		print "End of parsing get"
+
 	def processGet(self,t,d):
 		print "time: %s"%t
 		print "device: %d"%d
@@ -46,13 +47,13 @@ class MyHandler(BaseHTTPRequestHandler):
 			);	
 		content =urllib.urlopen(url=(URL.format(DEVICES[d])),data=data).read()
 		return content
-#		print content
-#		print "asasasas"
-#		print content[5]
 					
 	
 	def log_request(self, code=None, size=None):
 		print('Req')
+
+
+
 if __name__ == "__main__":
 	try:
 		server = HTTPServer((HOST, PORT), MyHandler)
