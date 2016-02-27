0b33bcb47edf6416e1d16272ce1541504bc45c5a
suteerth <svishnu@umd.edu>
2016-02-27 03:22:45 -0500

    trigger complete

--- Communications/triggerServer.py/httpTriggerServer.py
+++ Communications/triggerServer.py/httpTriggerServer.py
@@ -3,8 +3,14 @@ import urlparse
 import urllib
 import urllib2
 from urllib2 import urlopen, HTTPError
+import json
 
-URL="https://api.particle.io/v1/devices/51ff6f065082554910260887/led/"
+
+HOST='0.0.0.0'
+PORT=8080
+
+URL="https://api.particle.io/v1/devices/{!s}/led/"
+DEVICES = ['51ff6f065082554910260887']
 ACCESS_TOKEN="2228662ce1e5a04e7fb21f9b81aa3bc390b72506"
 COMMAND = "command=on"
 class MyHandler(BaseHTTPRequestHandler):
@@ -16,29 +22,40 @@ class MyHandler(BaseHTTPRequestHandler):
 			path, tmp = path.split('?')
 			qs = urlparse.parse_qs(tmp)
 		param = qs['params'][0].split(',')
-		time   = int(param[0])
+		time   = (param[0])
 		device = int(param[1])
-		self.processGet(time,device)		
+		con = self.processGet(time,device)
+		print con
+		self.send_response(200)
+		
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		self.wfile.write("<html><head><title>Title goes here.</title></head>")
+		self.wfile.write("<body><p>%s</p>"%con)
+		self.wfile.write("</body></html>")
 		print qs
 	
 		print "End of parsing get"
 	def processGet(self,t,d):
-		print "time: %d"%t
+		print "time: %s"%t
 		print "device: %d"%d
 		data = urllib.urlencode({
 			'access_token' : ACCESS_TOKEN,
-			'command':'on'
+			'command':t
 			}
 			);	
-		content =urllib.urlopen(url=URL,data=data).read()
-		print content
+		content =urllib.urlopen(url=(URL.format(DEVICES[d])),data=data).read()
+		return content
+#		print content
+#		print "asasasas"
+#		print content[5]
 					
 	
 	def log_request(self, code=None, size=None):
 		print('Req')
 if __name__ == "__main__":
 	try:
-		server = HTTPServer(('localhost', 8080), MyHandler)
+		server = HTTPServer((HOST, PORT), MyHandler)
 		print('Started http server')
 		server.serve_forever()
 	except KeyboardInterrupt:
